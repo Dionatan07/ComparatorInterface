f26a313354f34f7acab30400b7dced5f085a4c7d
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-07-16 12:56:07 -0300

    Use of Comparator Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,24 +2,45 @@ package application;
 
 import entities.Product;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
 
-
         List<Product> list = new ArrayList<>();
 
-        list.add(new Product("TV", 400.00));
-        list.add(new Product("Refrigerator", 600.00 ));
-        list.add(new Product("Sofa", 2000.00 ));
+        String filePath = "C:\\temp\\products.txt";
 
-        list.sort(new MyComparator());
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            String line = br.readLine();
+
+            while (line != null){
+                String[] fields = line.split(";");
+
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+
+                Product product = new Product(name, price);
+
+                list.add(product);
+
+                line = br.readLine();
+
+            }
+
+            list.sort(new MyComparator());
 
 
-        for (Product p : list){
-            System.out.println("Product: " + p.getName() + ", Price: " + String.format("%.2f", p.getPrice()));
+            for (Product p : list) {
+                System.out.println("Product: " + p.getName() + ", Price: " + String.format("%.2f", p.getPrice()));
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
         }
     }
 }
